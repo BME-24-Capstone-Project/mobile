21f09367a50ba83cb14da0edd7ba2dfdde1c4c0d
Adam Courchesne <courchesneadam@gmail.com>
2024-03-04 22:46:05 -0500

    Comitting to try something in a new branch.

--- components/currentDay/CurrentDayScreen.tsx
+++ components/currentDay/CurrentDayScreen.tsx
@@ -1,13 +1,34 @@
 import React = require("react");
-import { Button, SafeAreaView, Text, TextInput, View } from "react-native";
+import { SafeAreaView, Text, TextInput, View } from "react-native";
+import { Button } from "react-native-paper";
 
 export const CurrentDayScreen = ({navigation, route}) => {
-  const [username, setUsername] = React.useState("");
-  const [password, setPassword] = React.useState("");
+
+  const exercises = [
+    {
+      name: "One legged stair climb",
+      description: "Description here",
+      repetitions: 10,
+      sets: 3,
+    },
+    {
+      name: "Sit to stand",
+      description: "Description here",
+      repetitions: 10,
+      sets: 3,
+    },
+    {
+      name: "Walking",
+      description: "Description here",
+      repetitions: 10,
+      sets: 3,
+    },
+  ]
 
   return (
     <SafeAreaView>
-      <Text>Let's Exercise {route.params.username}!</Text>
+      <Text>Let's Exercise</Text>
+      <Button style={{width: '75%', margin: 10}}  mode="contained" buttonColor="green"  onPress={() => console.log("Start Exercies")}> Start Session </Button>
     </SafeAreaView>
   );
 };
--- components/login/LoginScreen.tsx
+++ components/login/LoginScreen.tsx
@@ -1,17 +1,48 @@
 import React = require("react");
 import { Alert, SafeAreaView } from "react-native";
 import { Button, TextInput } from "react-native-paper";
+import axios from 'axios';
+// import {decode as atob, encode as btoa} from "base-64"
+
 
 export const LoginScreen = ({navigation}) => {
+
   const [username, setUsername] = React.useState("");
   const [password, setPassword] = React.useState("");
   const [loginError, setLoginError] = React.useState("");
 
+
   const login = () => {
-    if (username === "acourche" && password === "test") {
-      console.log(username, password);
+    // if (username === "acourche" && password === "test") {
+    //   console.log(username, password);
+    //   navigation.navigate('CurrentDayScreen', {username: username})
+    // } else {
+    //   setLoginError("Incorrect username or password.")
+    //   Alert.alert(
+    //     'Login Error',
+    //     loginError,
+    //     [
+    //       {
+    //         text: 'Ok',
+    //         style: 'cancel',
+    //       },
+    //     ],
+    //     {
+    //       cancelable: true,
+    //     },
+    //   );
+    // }
+    axios.post(`http://localhost:8080/auth/login`,{}, {  
+      auth: {
+        username,
+        password
+      }
+    }).then((response) => {
+      console.log(response)
+      console.log(response.data);
       navigation.navigate('CurrentDayScreen', {username: username})
-    } else {
+    }).catch((error) => {
+      console.log(error);
       setLoginError("Incorrect username or password.")
       Alert.alert(
         'Login Error',
@@ -26,7 +57,13 @@ export const LoginScreen = ({navigation}) => {
           cancelable: true,
         },
       );
-    }
+    })
+
+    // axios.get(`http://localhost:8080/auth/test`).then((response) => {
+    //   console.log(response.data);
+    // }).catch((error) => {
+    //   console.log(error);
+    // })
   };
 
   return (
